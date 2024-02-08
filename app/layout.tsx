dc39ba8b9d45dcc25403de37538d5b606aaf4a71
Cristian-DW <cristtiiank@gmail.com>
2024-02-08 17:20:01 -0500

    chore: :construction_worker: out code parts and non-important files
    removing parts of code and unimportant files and conditioning of the work area such as parts of layout and pages in addition to cleaning styles that come by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "E-shop",
+  title: "E-Shop",
   description: "E-commerce app",
 };
 
