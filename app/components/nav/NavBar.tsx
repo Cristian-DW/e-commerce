51b804c919993a41bca757d787ea3c6776d3679a
Cristian-DW <cristtiiank@gmail.com>
2024-02-14 17:02:01 -0500

    feat: :art: creation of the navigation bar and code optimization
    Refactoring and improvement of the navigation bar  In this revision, I made a number of changes to the NavBar component to improve its readability and performance. Replaced inline style classes with tailwindcss for better code organization and maintenance. Additionally, the Container component was applied to wrap the navigation bar and semantic tags were implemented for better accessibility. Fixed typos and adjusted styles for a more consistent look. In addition, search, cart counter and user menu functions were incorporated. Optimized the use of the 'Redressed' font to reduce page load. This version provides an improved user experience and cleaner code.

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,9 +1,52 @@
+import Container  from "../Container";
+import Link from "next/link";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
+
 const NavBar = () => {
-   return (
-      <>
-           
-      </>
-   )
-}
+  return (
+    <div
+      className="
+      sticky
+      top-0
+      w-full
+      bg-slate-200
+      z-30
+      shadow-sm
+      "
+    >
+      <div
+        className="
+         py-4
+         border-b-[1px]"
+      >
+        <Container>
+          '
+          <div
+            className="
+               flex
+               items-center
+               justify-between
+               gap-3
+               md:gap-0
+               "
+          >
+            <Link href="/" className={`${redressed.className} text-3xl font-bold`}>E-Shop</Link>
+            <div className="hiden md:block"> Search</div>
+            <div
+              className="
+                  flex items-center gap-8md:gap-12
+                  "
+               >
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
