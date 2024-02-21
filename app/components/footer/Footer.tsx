18e9de065ef51127b2765cb0dc7d946ee31cb575
Cristian-DW <cristtiiank@gmail.com>
2024-02-21 15:03:39 -0500

    feat: :art: Finished creating the Footer with assistance from the previously developed FooterList component.
    Completed the full implementation of the Footer by utilizing the previously created FooterList component, providing comprehensive functionality and styling for the footer section.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,10 +1,56 @@
-const Footer = () => {
-   return (
-      <footer className="bg-slate-700 py-4 text-slate-200 text-sm mt-16">
+import Link from "next/link";
+import Container from "../container";
+import FooterList from "./FooterList";
+import {MdFacebook} from "react-icons/md"
+import {BsInstagram} from "react-icons/bs"
+import { AiFillTwitterCircle, AiFillYoutube } from "react-icons/ai";
 
-         
-      </footer>
-   )
-}
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 py-4 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href="#">Phones</Link>
+            <Link href="#">Laptops</Link>
+            <Link href="#">Destops</Link>
+            <Link href="#">TV's</Link>
+            <Link href="#">Accesories</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Services</h3>
+            <Link href="#">Contact Us</Link>
+            <Link href="#">Shipping Policy</Link>
+            <Link href="#">Returns & Exchanges</Link>
+            <Link href="#">Watches</Link>
+            <Link href="#">FAQs</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Follow Us</h3>
+            <Link href="#" className="flex items-center gap-2">
+              <MdFacebook size={20} />
+              Facebook
+            </Link>
+            <Link href="#" className="flex items-center gap-2">
+              <AiFillTwitterCircle size={20} />
+              Twitter
+            </Link>
+            <Link href="#" className="flex items-center gap-2">
+              <BsInstagram size={20} />
+              Instagram
+            </Link>
+            <Link href="#" className="flex items-center gap-2">
+              <AiFillYoutube size={20} />
+              Youtube
+            </Link>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
