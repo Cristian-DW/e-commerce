0ca898b720cba0fc7db401fbc657c90bc8764c02
Cristian-DW <cristtiiank@gmail.com>
2024-02-20 19:00:37 -0500

    feat: :art: Added React Icons and implemented Footer component with FooterList.
    Implemented React Icons library and added Footer component along with FooterList for enhanced UI and iconography in the application.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
 const Footer = () => {
    return (
-      <>
-         footer
-      </>
+      <footer className="
+      bg-slate-700
+      py-4
+      text-slate-200
+      text-sm
+      mt-16
+      ">
+         
+         
+      </footer>
    )
 }
 
