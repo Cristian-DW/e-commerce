b25eceee1a849d9d3148f588f4de13c23e52e5be
Cristian-DW <cristtiiank@gmail.com>
2024-02-22 17:52:19 -0500

    feat: :art: Add HomeBanner component on page
    Introduce the HomeBanner component to display a promotional banner on the main page. The component includes a header with a summer sale message, a 50% discount notice, and a representative image. Next.js and the Image tag are utilized for optimized image loading.

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image"
+
 const HomeBanner = () => {
    return (
       <header className="relative bg-gradient-to-r from-indigo-400 to-indigo-700 mb-8 ">
          <section className="mx-auto px-8 pyu-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
-            <div>
-               <h1>Summer Sales</h1>
+            <div className="mb-8 md:mb-0 text-center">
+               <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Summer Sales</h1>
+               <p className="text-lg md: text-xl text-white mb-2">Enjoy  discounts on selected items</p>
+               <p className="text-2xl md: text-5xl text-yellow-500 font-bold">GET 50% OFF</p>
             </div>
-            <div></div>
+            <div className="w-1/3 relative aspect-video">
+               <Image src="/banner.png"  alt="banner" fill className="object-contain" />
+            </div>
          </section>
       </header>
    )
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
 
 export default function Home() {
   return (
-   <div>E-Shop</div>
+   <div className="p-8 ">
+    <Container>
+      <div>
+        <HomeBanner />
+      </div>
+    </Container>
+   </div>
   );
 }
