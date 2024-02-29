dac4b75f01ec247bf7738214fd3812dad9308b48
Cristian-DW <cristtiiank@gmail.com>
2024-02-29 16:10:11 -0500

    feat: :card_file_box: Add 'utils' folder with product information in JSON, introduce 'truncateText,' and apply grid for responsive display.
    Introduce a new folder named 'utils' housing a JSON file containing detailed product information, facilitating efficient data management and access. Additionally, implement a 'truncateText' function to limit text length, enhancing readability in various contexts.  On the user interface front, incorporate a grid system for optimizing the presentation of product information. This approach ensures a visually pleasing experience on both mobile devices and other formats, improving usability and aesthetics. Exhaustive testing has been conducted to verify the effectiveness and responsiveness of these enhancements. This commit represents a significant step towards a more organized structure and an enhanced user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { trucateText } from "@/utils/trucateText";
 
 export default function Home() {
   return (
@@ -7,6 +9,11 @@ export default function Home() {
     <Container>
       <div>
         <HomeBanner />
+      </div>
+      <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {products.map((product: any) => {
+          return <div>{trucateText(product.name)}</div>
+        })}
       </div>
     </Container>
    </div>
