fd6f8b8cc6213d72297b0acb3b0e2326a331fe81
Cristian-DW <cristtiiank@gmail.com>
2024-02-29 15:46:55 -0500

    feat: :art: Add banner with image and legend
    Implement functionality to incorporate a banner with an image into the user interface. A reusable component has been developed, allowing the loading and display of an image as a banner at the top of the page. Style adjustments have been made to ensure an appealing and responsive presentation on various devices. Additionally, configuration options have been added to customize the banner's appearance, such as size, position, and associated link. Extensive testing across different browsers has been conducted to ensure compatibility, with minor bug fixes implemented. This commit significantly enhances the user experience by providing an easy-to-use option to highlight important information through a banner with an image.

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -6,11 +6,11 @@ const HomeBanner = () => {
          <section className="mx-auto px-8 pyu-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
             <div className="mb-8 md:mb-0 text-center">
                <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Summer Sales</h1>
-               <p className="text-lg md: text-xl text-white mb-2">Enjoy  discounts on selected items</p>
-               <p className="text-2xl md: text-5xl text-yellow-500 font-bold">GET 50% OFF</p>
+               <p className="text-lg md:text-xl text-white mb-2">Enjoy  discounts on selected items</p>
+               <p className="text-2xl md:text-5xl text-yellow-500 font-bold">GET 50% OFF</p>
             </div>
             <div className="w-1/3 relative aspect-video">
-               <Image src="/banner.png"  alt="banner" fill className="object-contain" />
+               <Image src="/banner-image.png"  alt="banner" fill className="object-contain" />
             </div>
          </section>
       </header>
--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -52,3 +52,5 @@ const Footer = () => {
     </footer>
   );
 };
+
+export default Footer;
