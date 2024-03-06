0afc7048a59e6d82703277de2ded1d4a54c6774a
Cristian-DW <cristtiiank@gmail.com>
2024-03-05 20:28:46 -0500

    feat: :sparkles: Update Next.js configuration and add imports in ProductCards
    I've made changes to the Next.js configuration to [provide context on the specific changes made, e.g., optimize build settings, adjust server configurations, etc.]. This ensures that our Next.js application is properly configured to meet our current requirements and optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
 import { trucateText } from "@/utils/trucateText";
+import ProductCards from "./components/products/ProductCards";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </div>
       <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {products.map((product: any) => {
-          return <div>{trucateText(product.name)}</div>
+          return <ProductCards data={product}/>
         })}
       </div>
     </Container>
