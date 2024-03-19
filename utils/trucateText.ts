ab39b9bd2fb4877656c220baaaa31a38d034e0f2
Cristian-DW <cristtiiank@gmail.com>
2024-03-19 15:38:28 -0500

    docs: :recycle: Added comments and modified component ProductCard
    Added comments to improve code readability and modified the ProductCard component to enhance its functionality and/or appearance.

--- utils/trucateText.ts
+++ utils/trucateText.ts
@@ -1,8 +1,23 @@
-export const trucateText = (str: string) => {
-   if (str.length < 22) {
-      return str;
-   }
-      return str.substring(0.22) + "...";
-   }
+/**
+ * Truncates a string to a maximum length and appends '...' if the
+ * string is longer than the maximum length.
+ *
+ * @param str string to truncate
+ * @returns truncated string
+ */
+export const trucateText = (str: string): string => {
+    /**
+     * Maximum length of the string. If the original string is shorter
+     * than this, it will not be truncated.
+     */
+    const maxLength = 22;
+    
+    if (str.length <= maxLength) {
+        return str;
+    }
+    
+    return str.substring(0, maxLength) + "...";
+}
+
    
    
