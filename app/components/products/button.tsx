7b018bd2b9acaa00c090cdd230e6bfd26b765525
Cristian-DW <cristtiiank@gmail.com>
2024-04-10 13:07:02 -0500

    feat: :sparkles: Add Button component
    Created a reusable Button component that accepts props to customize its appearance and behavior. The component includes options for label, disabled state, outlined style, small size, custom styles, and an optional icon. This component can be easily integrated into various parts of the project to provide consistent button functionality.

---
+++ app/components/products/button.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { IconType } from "react-icons"
+
+interface ButtonProps {
+   label: string,
+   disabled?: boolean,
+   outlined?: boolean,
+   small?: boolean
+   custom?: string,
+   icon?: IconType,
+   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button: React.FC<ButtonProps> = ({
+   label,
+   disabled,
+   outlined,
+   small,
+   custom,
+   icon: Icon,
+   onClick
+
+}) => {
+
+   return (
+      <button
+      disabled={disabled} 
+      className={`relative w-full bg-slate-800 text-white px-4 py-3 rounded-md text-sm font-medium hover:bg-slate-900 disabled:opacity-50 disabled:cursor-not-allowed
+      ${outlined ? 'border border-slate-300' : 'bg-slate-800'}
+      ${outlined ? 'text-slate-300' : 'text-white'}
+      ${small ? 'text-sm font-light' : 'text-md font-semibold'}
+      ${small ? 'py-1 px-2 border-[1px]' : 'py-3 px-4 border-[2px]'}
+      ${custom ? custom : ''}
+      `}>
+         {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+         {label}
+      </button>
+   )
+}
