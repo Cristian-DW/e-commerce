5b8f1721be27d273bef7da3a18a3f72dac51ff03
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-05-24 20:49:55 -0500

    docs(Cart): ✏️ add documentation comments to Cart component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,16 +1,27 @@
 import Container from "../components/Container";
 import CartClient from "./CartClient";
 
+/**
+ * Cart Component
+ * 
+ * This component serves as the main container for the shopping cart page. It uses the `Container`
+ * component to provide a consistent layout and includes the `CartClient` component to handle
+ * the display and functionality of the shopping cart.
+ * 
+ * @component
+ * @example
+ * return (
+ *   <Cart />
+ * )
+ */
 const Cart = () => {
-
-   return (
-      <div className="pt-8">
-         <Container>
-            <CartClient/>
-         </Container>
-
-      </div>
-   );
+  return (
+    <div className="pt-8">
+      <Container>
+        <CartClient />
+      </Container>
+    </div>
+  );
 }
 
- export default Cart;
+export default Cart;
