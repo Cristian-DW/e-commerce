1e23e343a58cdaca8be51b3227ebfc829c158657
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-05-27 22:40:25 -0500

    docs(FooterList): ✏️ add documentation comments to FooterList component

--- app/components/footer/FooterList.tsx
+++ app/components/footer/FooterList.tsx
@@ -1,24 +1,43 @@
 interface FooterListProps {
-   children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-
-const FooterList : React.FC<FooterListProps> = ({children}) => {
+/**
+ * FooterList Component
+ *
+ * This component serves as a container for lists within the footer.
+ * It is used to group related links together.
+ *
+ * @component
+ * @example
+ * return (
+ *   <FooterList>
+ *     <h3>Category</h3>
+ *     <Link href="#">Item 1</Link>
+ *     <Link href="#">Item 2</Link>
+ *   </FooterList>
+ * )
+ * 
+ * @param {FooterListProps} props - The properties for the FooterList component.
+ * @param {React.ReactNode} props.children - The child elements to be rendered inside the FooterList.
+ * 
+ * @returns {JSX.Element} The rendered FooterList component
+ */
+const FooterList: React.FC<FooterListProps> = ({ children }) => {
   return (
-   <div className="
-   w-full
-   sm:w-1/2
-   md:w-1/4
-   lg:w-1/6
-   mb-6
-   flex
-   flex-col
-   gap-2
-   ">
+    <div className="
+      w-full
+      sm:w-1/2
+      md:w-1/4
+      lg:w-1/6
+      mb-6
+      flex
+      flex-col
+      gap-2
+    ">
       {children}
-   </div>
-  )
- 
+    </div>
+  );
 };
 
 export default FooterList;
