f08ae14faad2d22291ec023b45d3e3d0df0e5338
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-06-11 20:25:42 -0500

    docs: 📝 add documentation comments to CartProvider component
    Added detailed documentation comments to the CartProvider component to improve code readability and maintainability. The comments describe the purpose of the component and its props.

--- provider/CartProvider.tsx
+++ provider/CartProvider.tsx
@@ -2,15 +2,26 @@
 
 import { CartContextProvider } from "@/hooks/UseCart";
 
+/**
+ * Props for the CartProvider component.
+ */
 interface CartProviderProps {
-   children: React.ReactNode
+   /**
+    * The child elements to be wrapped by the CartProvider.
+    */
+   children: React.ReactNode;
 }
 
-const CartProvider: React.FC<CartProviderProps> = ({children}) => {
-
+/**
+ * A provider component that wraps its children with the CartContextProvider.
+ * This allows any descendant components to access the cart context.
+ * @param props The properties for the component, including children elements.
+ * @returns The CartContextProvider wrapping the children elements.
+ */
+const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
    return (
-      <CartContextProvider> {children}</CartContextProvider>
-   )
+      <CartContextProvider>{children}</CartContextProvider>
+   );
 }
 
 export default CartProvider;
